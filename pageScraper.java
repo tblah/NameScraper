81910f69a664a0930533c5cc5df040d175358dd1
Tom Eccles <t@freedommail.info>
2014-10-03 19:13:03 +0100

    Forgot GPL3 header
    Added GPL3 Header to pageScraper.java

--- pageScraper.java
+++ pageScraper.java
@@ -1,3 +1,24 @@
+/*
+ NameScraper - a utility to look up the name of a member of the University
+ Of Southampton ECS Department using their email id. 
+ Copyright (C) 2014 Tom Eccles
+ 
+ This file is part of NameScraper.
+ 
+ NameScraper is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ NameScraper is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with NameScraper.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -60,7 +81,6 @@ public class pageScraper {
 				}
 				
 				if (tagMatcher.find()) {
-					System.out.println("match");
 					tag = currLine.substring(tagMatcher.start(), tagMatcher.end());
 					break;
 				}
