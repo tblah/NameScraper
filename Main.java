727f9a4175666fd05869cc3c3a9a667431b5397a
Tom Eccles <t@freedommail.info>
2014-10-03 15:53:24 +0100

    Eclipse initial commit
    Only change is to add my main class and a GPL3 header

---
+++ Main.java
@@ -0,0 +1,29 @@
+/*
+ NameScraper - a utility to look up the name of a member of the University
+ Of Southampton ECS Department using their email id. 
+ Copyright (C) 2014 Tom Eccles
+ 
+ This file is part of NameScraper.
+ 
+ NameScraper is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ NameScraper is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with NameScraper.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
